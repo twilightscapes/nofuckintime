19f77b4826a2e676600b64fc11cbb944621ccd71
twilightscapes <ihatespam@gmail.com>
2021-11-07 20:17:49 -0600

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -152,7 +152,7 @@ function ShowSuggestion() {
   
 
   
-<div style={{width:'90%', maxWidth:'400px', margin:'0 auto 0 auto', fontSize:'90%', padding:'5px 0 ', border:'4px dotted', borderRadius:'12px', textAlign:'center', position:'relative', }}>
+<div style={{width:'90%', maxWidth:'400px', margin:'65px auto 0 auto', fontSize:'90%', padding:'5px 0 ', border:'4px dotted', borderRadius:'12px', textAlign:'center', position:'relative', }}>
 <IoArrowRedoSharp style={{position:'absolute', top:'0', left:'0', fontSize:'60px', transform: 'rotate(-45deg)', }} />
 <IoArrowUndoSharp style={{position:'absolute', top:'0', right:'0', fontSize:'60px', transform: 'rotate(45deg)', }} />
   
@@ -381,9 +381,9 @@ const iframeUrl = "https://www.youtube.com/embed/" + frontmatter.youtuber + ""
           playing
           playsinline
           playIcon={
-            <button aria-label="Click To Play" className="clickplay" style={{position:'relative', zIndex:'0', top:'', border:'0px  solid red', width:'100vw', height:'0', background:'transparent', color:'#fff', fontSize:'18px', textAlign:'center', display:'flex', flexDirection:'column', verticalAlign:'center', justifyContent:'center', alignItems:'center', paddingTop:'0', borderRadius:'12px'}}>
+            <button aria-label="Click To Play" className="clickplays" style={{position:'relative', zIndex:'0', top:'', border:'0px  solid red', width:'100vw', height:'', background:'transparent', color:'#fff', fontSize:'18px', textAlign:'center', display:'flex', flexDirection:'column', verticalAlign:'center', justifyContent:'center', alignItems:'center', paddingTop:'0', borderRadius:'12px'}}>
           
-        <div className="" style={{position:'absolute', top:'-130px', zIndex:'0', textAlign:'center', animation:'fadeIn 3s', display:'flex', justifyContent:'center', width:'auto', marginBottom:''}}>
+        <div className="" style={{position:'absolute', top:'-70px', zIndex:'0', textAlign:'center', animation:'fadeIn 3s', display:'flex', justifyContent:'center', width:'auto', marginBottom:''}}>
           
       
           {/* <div className="" style={{fontSize:'14px', fontWeight:'', padding:'0 0 0 .3rem',}}>Click For Audio</div> */}
@@ -584,12 +584,7 @@ const { iconimage } = useSiteMetadata()
 {/* <br />
 <br /> */}
 
-{ !YouTube2 ? (
-            ""
-       
-          ) : (
-            <Iframer3 />
-          )}
+
 
 
 {Suggestion1 ? (
@@ -597,6 +592,13 @@ const { iconimage } = useSiteMetadata()
        
           ) : (
             ""
+          )}
+
+{ !YouTube2 ? (
+            ""
+       
+          ) : (
+            <Iframer3 />
           )}
 
 
