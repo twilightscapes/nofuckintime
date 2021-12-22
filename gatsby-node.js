0da1f244398145efc283fcdebd45a21464d1a33e
twilightscapes <ihatespam@gmail.com>
2021-12-21 23:02:52 -0600

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -102,3 +102,16 @@ exports.createSchemaCustomization = ({ actions }) => {
   `
   createTypes(typeDefs)
 }
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
